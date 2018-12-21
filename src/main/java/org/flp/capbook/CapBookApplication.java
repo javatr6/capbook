171f9977d64863642abe3449671c09fc3f39d0f0
MyTraining2018 <captrainingnov2016@gmail.com>
2018-12-21 10:42:10 +0530

    first commit

---
+++ src/main/java/org/flp/capbook/CapBookApplication.java
@@ -0,0 +1,14 @@
+package org.flp.capbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapBookApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/flp/capbook/CapBookApplicationTests.java
@@ -0,0 +1,17 @@
+package org.flp.capbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapBookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
